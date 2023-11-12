859b2d9dfac87446d36586475e3ede77c61b78b3
Pockitt <simen.magnussen@outlook.com>
2023-11-12 21:11:53 +0100

    Error handling test 1

--- js/script.js
+++ js/script.js
@@ -12,25 +12,28 @@ import { getGames } from "./data/getGames.js";
 switch(location.pathname) {
   case "/collection":
     try {
-      renderCollection(favorites, newReleases, fifteen, parentMostPlayed, parentNewGames, allGames, heading1, heading2, heading3)
+       renderCollection(favorites, newReleases, fifteen, parentMostPlayed, parentNewGames, allGames, heading1, heading2, heading3)
     } catch (error) {
-      console.log("An error occurred!", error);
+      console.log(error);
+      parent.innerHTML = "Oops! An error occurred!";
     }
     break;
-  case "/product/gears-of-war-3.html":
+  case "/product/gears-of-war-3.":
     try {
         const id = getId();
         const newURL = url + "/" + id;
         const title = await getGames(newURL);
         renderGame(title);
     } catch (error) {
-      console.log("An error occurred!", error);
+        console.log(error);
+        gamePage.innerHTML = "Oops! An error occurred!";
     }
     break;
   default:
      try {
-       renderHome(newReleases, onSale, favorites, parentNewReleases, parentOnSale, parentFanFavorites, heading1Home, heading2Home, heading3Home);
+        renderHome(newReleases, onSale, favorites,   parentNewReleases, parentOnSale, parentFanFavorites,  heading1Home, heading2Home, heading3Home);
      } catch (error) {
-       console.log("An error occurred!", error);
+        console.log(error);
+        newReleases.innerHTML = "Oops! An error occurred!";
      }
 }
